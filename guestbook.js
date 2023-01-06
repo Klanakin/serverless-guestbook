266453c1237b0f6c1a28c67dda34d594786375fa
DK <61115895+Klanakin@users.noreply.github.com>
2023-01-06 22:13:15 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/don.klanakin%40gmail.com_dev/guestbook/read-guestbook-entries-sequence';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
